97bb016569f7898512b9070c76184ba48f887f7f
Simon Thörnqvist <simon@plug-n-trade.com>
2017-03-22 08:35:28 +0100

    match cmd args wip

--- src/main.ml
+++ src/main.ml
@@ -6,11 +6,18 @@ type tag = {
   minor: int;
   patch: int;
 }
-type sem_ver =
-  | Major | Minor | Patch
+type sem_ver = | Major | Minor | Patch
+
+let tag_to_string {major; minor; patch;} =
+  Printf.sprintf "%d.%d.%d" major minor patch
 
 let list_tags () =
   Shell.run_one ~expect:[0;128] "git" ["describe"; "--abbrev=0"]
+
+let git_tag message tag =
+  let opts = ["tag"; "-a"; tag_to_string tag; "-m "; message;] in
+  match Shell.run_one ~expect:[0] "git" opts with
+    _ -> tag
 
 let create_tag sep_tag =
   match List.map ~f:int_of_string (String.split ~on: '.' sep_tag) with
@@ -28,12 +35,13 @@ let tick_version ver t =
   | Minor -> { t with minor = t.minor + 1; patch = 0; }
   | Major -> { major = t.major + 1; minor = 0; patch = 0; }
 
-let next_version ver =
+let next_version ver message =
   let current_version = list_tags ()
                         |> determine_current_version
                         |> create_tag
   in
   tick_version ver current_version
+  |> git_tag message
 
 let major =
   Command.basic ~summary:"Major version"
@@ -44,7 +52,7 @@ let major =
     (fun message () ->
        match message with
        | Some buf -> printf "message %s" buf
-       | None -> printf "no message"
+       | None -> printf "version bump"
     )
 
 let minor =
@@ -56,7 +64,7 @@ let minor =
     (fun message () ->
        match message with
        | Some buf -> printf "message %s" buf
-       | None -> printf "no message"
+       | None -> printf "version bump"
     )
 
 let patch =
@@ -67,8 +75,12 @@ let patch =
     )
     (fun message () ->
        match message with
-       | Some buf -> printf "message %s" buf
-       | None -> printf "no message"
+       | Some buf -> next_version Patch buf
+                     |> tag_to_string
+                     |> printf "%s created"
+       | None -> next_version Patch "version bump"
+                 |> tag_to_string
+                 |> printf "%s created"
     )
 
 let command =
