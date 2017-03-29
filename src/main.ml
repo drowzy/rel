80b0242228761f42d18f3e744ad9d066c8a805b1
Simon Thörnqvist <simon@plug-n-trade.com>
2017-03-29 09:32:00 +0200

    can perform `git tag` from cli

--- src/main.ml
+++ src/main.ml
@@ -6,6 +6,7 @@ type tag = {
   minor: int;
   patch: int;
 }
+
 type sem_ver = | Major | Minor | Patch
 
 let tag_to_string {major; minor; patch;} =
@@ -15,14 +16,14 @@ let list_tags () =
   Shell.run_one ~expect:[0;128] "git" ["describe"; "--abbrev=0"]
 
 let git_tag message tag =
-  let opts = ["tag"; "-a"; tag_to_string tag; "-m "; message;] in
+  let opts = ["tag"; "-a"; tag_to_string tag; "-m " ^ message;] in
   match Shell.run_one ~expect:[0] "git" opts with
     _ -> tag
 
 let create_tag sep_tag =
   match List.map ~f:int_of_string (String.split ~on: '.' sep_tag) with
   | [major; minor; patch] -> { major = major; minor = minor; patch = patch; }
-  | [] | _ -> { major = 0; minor = 0; patch = 0; }
+  | _ -> { major = 0; minor = 0; patch = 0; }
 
 let determine_current_version tag =
   match tag with
@@ -35,13 +36,18 @@ let tick_version ver t =
   | Minor -> { t with minor = t.minor + 1; patch = 0; }
   | Major -> { major = t.major + 1; minor = 0; patch = 0; }
 
-let next_version ver message =
+let next_version version message =
+  let msg = match message with
+    | Some buf -> buf
+    | None -> "version bump"
+  in
   let current_version = list_tags ()
                         |> determine_current_version
                         |> create_tag
   in
-  tick_version ver current_version
-  |> git_tag message
+  tick_version version current_version
+  |> git_tag msg
+  |> tag_to_string
 
 let major =
   Command.basic ~summary:"Major version"
@@ -50,9 +56,8 @@ let major =
       +> flag "-m" (optional string) ~doc:"string message"
     )
     (fun message () ->
-       match message with
-       | Some buf -> printf "message %s" buf
-       | None -> printf "version bump"
+       next_version Minor message
+       |> printf "%s created"
     )
 
 let minor =
@@ -62,9 +67,8 @@ let minor =
       +> flag "-m" (optional string) ~doc:"string message"
     )
     (fun message () ->
-       match message with
-       | Some buf -> printf "message %s" buf
-       | None -> printf "version bump"
+       next_version Minor message
+       |> printf "%s created"
     )
 
 let patch =
@@ -74,13 +78,8 @@ let patch =
       +> flag "-m" (optional string) ~doc:"string message"
     )
     (fun message () ->
-       match message with
-       | Some buf -> next_version Patch buf
-                     |> tag_to_string
-                     |> printf "%s created"
-       | None -> next_version Patch "version bump"
-                 |> tag_to_string
-                 |> printf "%s created"
+       next_version Patch message
+       |> printf "%s created"
     )
 
 let command =
