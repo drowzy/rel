8f7be2e420163f934d08b64641ea2f6b6c0100c5
Simon Thörnqvist <simon@plug-n-trade.com>
2017-04-08 20:55:13 +0200

    preminor, prepatch

--- src/main.ml
+++ src/main.ml
@@ -25,26 +25,44 @@ let ext_cmd =
   )
 let run version = (fun message () ->
     next_version version message
-    |> printf "%s created"
+    |> printf "%s created\n"
   )
 
+let deter_pre pre =
+  match pre with
+    | Some buf -> Tag.Pre buf
+    | None -> Tag.Regular
+
 let major = Command.basic ~summary:"Major version" common (run (Tag.Major Tag.Regular))
+let minor = Command.basic ~summary:"Minor version" common (run (Tag.Minor Tag.Regular))
+let patch = Command.basic ~summary:"Patch version" common (run (Tag.Patch Tag.Regular))
+
 let pre_major =
   Command.basic
     ~summary:"Pre-major version"
     ext_cmd
-    (fun pre message () ->
-     let prefix = match pre with
-       | Some buf -> Tag.Pre buf
-       | None -> Tag.Regular
-     in
-     run (Tag.Major prefix) message ()
-    )
-let minor = Command.basic ~summary:"Minor version" common (run (Tag.Minor Tag.Regular))
-let patch = Command.basic ~summary:"Patch version" common (run (Tag.Patch Tag.Regular))
+    (fun pre message () -> run (Tag.Major (deter_pre pre)) message ())
+
+let pre_minor =
+  Command.basic
+    ~summary:"Pre-minor version"
+    ext_cmd
+    (fun pre message () -> run (Tag.Minor (deter_pre pre)) message ())
+
+let pre_patch =
+  Command.basic
+    ~summary:"Pre-patch version"
+    ext_cmd
+    (fun pre message () -> run (Tag.Patch (deter_pre pre)) message ())
 
 let command =
   Command.group ~summary: "Creates a semantic git tag"
-    [ "major", major; "minor", minor; "patch", patch; "premajor", pre_major; ]
+    [ "major", major;
+      "minor", minor;
+      "patch", patch;
+      "premajor", pre_major;
+      "preminor", pre_minor;
+      "prepatch", pre_patch;
+    ]
 
 let () = Command.run command
